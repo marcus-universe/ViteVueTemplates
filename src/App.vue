83d41be06c7023e394be35df26710531492a31bf
Marcus Tutorials <marcusharting@web.de>
2022-12-30 16:37:56 +0100

    Added Router Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const header = ref<HTMLElement>()
+
+onMounted(() => {
+  console.log(header.value)
+})
+</script>
 
 <template>
-  <header class="flex_c_h">
-    <div class="wrapper">
+  <header ref="header" class="flex_c_h">
+    <div class="wrapper flex_c_h align_c">
       <nav class="flex_c_h gap1 medium">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" class="text_center">Home</RouterLink>
+        <RouterLink to="/about" class="text_center">About</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.path" />
+    </transition>
+  </router-view>
 </template>
