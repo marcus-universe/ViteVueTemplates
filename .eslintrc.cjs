b5d96d097457af9e7a6633fcc93ba0b52826d9d9
Marcus Tutorials <marcusharting@web.de>
2022-12-30 14:57:16 +0100

    Add Sass and prettierrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-multiple-template-roots': 'off',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 6,
+        maxBOF: 0,
+        maxEOF: 0,
+      },
+    ],
+    'eol-last': ['off', 'always'],
+    'space-before-function-paren': ['off', 'always'],
+    'comma-dangle': ['off', 'always'],
+    indent: ['off', 6],
+    'no-trailing-spaces': [
+      'off',
+      {
+        skipBlankLines: true,
+      },
+    ],
+    'object-curly-spacing': ['off', 'always'],
+    quotes: ['off', 'double'],
+    'dot-notation': [
+      'off',
+      {
+        allowKeywords: true,
+      },
+    ],
+    'multi-word-component-names': [
+      0,
+      {
+        componentName: '^[a-zA-Z]+$',
+      },
+    ],
+    'vue/multi-word-component-names': [
+      'off',
+      {
+        componentName: '^[a-zA-Z]+$',
+      },
+    ],
+    'no-dupe-keys': ['off'],
+    'vue/keyword-spacing': 'warn',
+    'eslint-disable-next-line': 'off',
+    'eslint-disable': 'off',
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="flex_c_h">
+    <div class="wrapper">
+      <nav class="flex_c_h gap1 medium">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
